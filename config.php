19e5b56a4bcf1224aa0518acfbd6c66dec37f737
SaretoTiyer <reyestorressanti@gmail.com>
2025-12-02 16:47:51 -0500

    update

--- config.php
+++ config.php
@@ -1,15 +1,19 @@
 <?php
+// Recupera las variables de entorno de Railway
+$servername = getenv('MYSQL_HOST');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+$port = getenv('MYSQL_PORT');
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "api_ruleta";
+// Crear conexión usando las variables de entorno
+// El puerto debe pasarse como un parámetro separado en la mayoría de los casos
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-
+// Verificar conexión
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-
-$conn->set_charset("utf8");
+// Opcional: Establecer el juego de caracteres
+$conn->set_charset("utf8mb4");
 ?>
